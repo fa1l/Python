28a6ef9fcc5a98ddc126b698dde464add2e9a07b
fa1l <fa1l@users.noreply.github.com>
2020-10-08 16:19:55 +0500

    get rid of empty solution function

--- project_euler/problem_63/sol1.py
+++ project_euler/problem_63/sol1.py
@@ -1,6 +1,6 @@
+
 """
 https://projecteuler.net/problem=63
-
 The 5-digit number, 16807=7**5, is also a fifth power. Similarly, the 9-digit number,
 134217728=8**9, is a ninth power.
 How many n-digit positive integers exist which are also an nth power?
@@ -13,16 +13,16 @@ Using these conclusions, we will calculate the result.
 """
 
 
-def compute_nums(max_base: int, max_power: int) -> int:
+def solution(max_base: int = 10, max_power: int = 22) -> int:
     """
     Returns the count of all n-digit numbers which are nth power
-    >>> compute_nums(10, 22)
+    >>> solution(10, 22)
     49
-    >>> compute_nums(0, 0)
+    >>> solution(0, 0)
     0
-    >>> compute_nums(1, 1)
+    >>> solution(1, 1)
     0
-    >>> compute_nums(-1, -1)
+    >>> solution(-1, -1)
     0
     """
     bases = range(1, max_base)
@@ -30,11 +30,6 @@ def compute_nums(max_base: int, max_power: int) -> int:
     return sum(
         1 for power in powers for base in bases if len(str((base ** power))) == power
     )
-
-
-def solution(max_base: int = 10, max_power: int = 22) -> int:
-    """Returns the count of all n-digit numbers which are nth power."""
-    return compute_nums(max_base, max_power)
 
 
 if __name__ == "__main__":
